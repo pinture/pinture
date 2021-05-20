d146e4b7da40c1dd621b4d2f5d13769362c77acf
Steve Liu <chiruei56@gmail.com>
2021-05-20 23:58:35 +0800

    feat: add cancellation to contract & test

--- contracts/Pinture.sol
+++ contracts/Pinture.sol
@@ -10,21 +10,26 @@ contract Pinture {
     address private licenseTokenAddr;
 
     mapping(uint256 => uint256) private _tokenIdToPrice;
-    
+    mapping(uint256 => uint256) private _allTokensIndex;
+
     uint256[] private _allTokens;
 
-    event Buy(uint256 tokenId , address buyer, uint256 price);
-    event SetPrice(address owner, uint256 tokenId , uint256 price);
-    
+    event Buy(uint256 tokenId, address buyer, uint256 price);
+    event SetPrice(address owner, uint256 tokenId, uint256 price);
+    event Cancel(uint256 tokenId);
+
     constructor(IERC721 licenseToken) {
         _token = licenseToken;
     }
 
-    function getListedTokens() public view returns( uint256[] memory ){
+    // dont use!
+    function getListedTokens() public view returns (uint256[] memory) {
         return _allTokens;
     }
+
     function setPrice(uint256 tokenId, uint256 price) public _checkApproval(tokenId) {
         _tokenIdToPrice[tokenId] = price;
+        _allTokensIndex[tokenId] = _allTokens.length;
         _allTokens.push(tokenId);
 
         emit SetPrice(msg.sender, tokenId, price);
@@ -34,17 +39,35 @@ contract Pinture {
         return _tokenIdToPrice[tokenId];
     }
 
+    function cancel(uint256 tokenId) public _checkApproval(tokenId) {
+        require(_token.ownerOf(tokenId) == msg.sender, "You are not the owner of the token.");
+        delete _tokenIdToPrice[tokenId];
+        
+        uint256 lastTokenIndex = _allTokens.length - 1;
+        uint256 tokenIndex = _allTokensIndex[tokenId];
+
+        uint256 lastTokenId = _allTokens[lastTokenIndex];
+
+        _allTokens[tokenIndex] = lastTokenId; 
+        _allTokensIndex[lastTokenId] = tokenIndex;
+
+        delete _allTokensIndex[tokenId];
+        _allTokens.pop();
+
+        emit Cancel(tokenId);
+    }
+
     function buy(uint256 tokenId) external payable _checkApproval(tokenId) {
-        require( _tokenIdToPrice[tokenId] == msg.value, "The given ether is not matching the price" );
+        require(_tokenIdToPrice[tokenId] == msg.value, "The given ether is not matching the price");
 
         address ownerOfToken = _token.ownerOf(tokenId);
 
-        _token.safeTransferFrom(ownerOfToken, msg.sender , tokenId);
-        emit Buy(tokenId, msg.sender , msg.value);
-    }   
+        _token.safeTransferFrom(ownerOfToken, msg.sender, tokenId);
+        emit Buy(tokenId, msg.sender, msg.value);
+    }
 
     modifier _checkApproval(uint256 tokenId) {
-        require(address(this) == _token.getApproved(tokenId), "This token haven't been approved for Pinture yet." );
+        require(address(this) == _token.getApproved(tokenId), "This token haven't been approved for Pinture yet.");
         _;
-    } 
+    }
 }
--- test/PhotoToken.ts
+++ test/PhotoToken.ts
@@ -51,28 +51,33 @@ describe("Deploy contract and mint a token",function () {
             expect(await this.licenseToken.connect(this.signers.admin).getPictureTokenAddress()).to.equal(this.pictureToken.address)
         })
     
-        it("Mint 1 usage token of photo token to photographer", async function () {
+        it("Mint 2 license token to photographer", async function () {
             const imageHash = "QmPXX1FFHHVYMexKAXLauDSA13XTFGnxe7wT6xsqWRcLy5";
             const photographer:string = await this.signers.photographer.getAddress();
             const picTokenOfPhotographerBy0: BigNumber = await this.pictureToken.connect(this.signers.admin).tokenOfOwnerByIndex(photographer, 0);
 
             const mintRes = await this.licenseToken.connect(this.signers.admin).safeMint(photographer,picTokenOfPhotographerBy0,1620922528, 1620923528, 100, imageHash, 100 );
+            const mintRes2 = await this.licenseToken.connect(this.signers.admin).safeMint(photographer,picTokenOfPhotographerBy0,1620922128, 1620925528, 100, imageHash, 105 );
+
             const licTokenOfPhotographerBy0: BigNumber = await this.licenseToken.connect(this.signers.admin).tokenOfOwnerByIndex(photographer, 0);
             const balanceOf:BigNumber = await this.licenseToken.connect(this.signers.admin)["balanceOf(address)"](photographer);
             const uri:string = await this.licenseToken.connect(this.signers.admin)["tokenURI(uint256)"](licTokenOfPhotographerBy0);
 
-            expect(balanceOf).to.equal(BigNumber.from("0x1"));
+            expect(balanceOf).to.equal(BigNumber.from("0x2"));
             expect(uri).to.equal(`ipfs://${imageHash}`);
         });
 
         it("get all license token from account", async function () {
             const photographer:string = await this.signers.photographer.getAddress();
             const tokenOfOwnerByIndex: BigNumber = await this.licenseToken.connect(this.signers.admin).tokenOfOwnerByIndex(photographer, 0);
+            const tokenOfOwnerByIndex1: BigNumber = await this.licenseToken.connect(this.signers.admin).tokenOfOwnerByIndex(photographer, 1);
+
             const totalSup: BigNumber = await this.licenseToken.connect(this.signers.admin).totalSupply();
             const tokenByIx: BigNumber = await this.licenseToken.connect(this.signers.admin).tokenByIndex(0);
 
             expect(tokenOfOwnerByIndex).to.equal(BigNumber.from("0x36054b77b837c9ab"));
-            expect(totalSup).to.equal(BigNumber.from(1));
+            expect(tokenOfOwnerByIndex1).to.equal(BigNumber.from("0x84eab3c18994358b"));
+            expect(totalSup).to.equal(BigNumber.from(2));
             expect(tokenByIx).to.equal(BigNumber.from("0x36054b77b837c9ab"));
         })
 
@@ -81,13 +86,19 @@ describe("Deploy contract and mint a token",function () {
             this.pinture = <Pinture>await deployContract(this.signers.admin, pintureArtifact, [this.licenseToken.address]);
         }); 
 
-        it("Photographer sell license on the market", async function(){
+        it("Photographer sell 2 licenses on the market", async function(){
             const licenseId: BigNumber = BigNumber.from("0x36054b77b837c9ab");
             const approval = await this.licenseToken.connect(this.signers.photographer).approve(this.pinture.address,licenseId);
             const setPrice = await this.pinture.connect(this.signers.photographer).setPrice(licenseId, 1);
             const getPrice = await this.pinture.connect(this.signers.photographer).getPrice(licenseId);
 
+            const licenseId2: BigNumber = BigNumber.from("0x84eab3c18994358b");
+            const approval2 = await this.licenseToken.connect(this.signers.photographer).approve(this.pinture.address,licenseId2);
+            const setPrice2 = await this.pinture.connect(this.signers.photographer).setPrice(licenseId2, 100);
+            const getPrice2 = await this.pinture.connect(this.signers.photographer).getPrice(licenseId2);
+
             expect(getPrice).to.equal(BigNumber.from(1));
+            expect(getPrice2).to.equal(BigNumber.from(100));
         })
 
         it("User buy it from the market", async function(){
@@ -97,11 +108,18 @@ describe("Deploy contract and mint a token",function () {
             const allTokens = await this.pinture.getListedTokens();
 
             expect(ownerOfLic).to.equal(this.signers.user.address);
-            expect(allTokens.length).to.equal(1);
+            expect(allTokens.length).to.equal(2);
             expect(allTokens[0]).to.equal(BigNumber.from("0x36054b77b837c9ab"));
         })
 
-    })
+        it("Photographer cancel the sell of license token", async function () { 
+            const licenseId: BigNumber = BigNumber.from("0x84eab3c18994358b");
+            const cancel = await this.pinture.connect(this.signers.photographer).cancel(licenseId);
+            const disapproval = await this.licenseToken.connect(this.signers.photographer).approve( hre.ethers.constants.AddressZero, licenseId );
+            const allTokens = await this.pinture.getListedTokens();
 
+            expect(allTokens.length).to.equal(1);
+        })
+    })
 })
 
